b54b034d7b2dbacab364a89917d741e54df4e614
ThamaraiKannan1496 <kannanbala1496@gmail.com>
2024-10-01 16:26:23 +0530

    asdd

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/">
-          <Route path="/" element={<Home />}></Route>
+        <Route path="/Moderna-vite">
+          <Route path="/home" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
         </Route>
       </Routes>
