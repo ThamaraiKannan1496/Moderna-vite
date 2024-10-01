161814ddf1dc7ad50beee387b10934f6bbb3bfd2
ThamaraiKannan1496 <kannanbala1496@gmail.com>
2024-10-01 16:17:06 +0530

    sdf

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/Moderna-vite">
+        <Route path="/">
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
         </Route>
