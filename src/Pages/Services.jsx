10ca467cec7707de9b80a59a5d0605bc6e135c99
krishnakumar-M04 <krishnakumarm432@gmail.com>
2024-10-01 17:57:50 +0530

    kk-serve

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
 const Services = () => {
-  return <div>service Section</div>;
-     <div>main section</div>
+  return<> <div>service Section</div>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis consectetur vero et esse necessitatibus animi voluptate perspiciatis inventore officia. Veritatis, velit? Tempora eius voluptatem deserunt asperiores blanditiis. At, suscipit quasi!</p></>;
+    
 };
 
 export default Services;
