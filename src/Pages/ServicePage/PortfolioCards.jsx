14bd4c020117697d8ef17c0df13eb0ff6d2e0b7c
ThamaraiKannan1496 <kannanbala1496@gmail.com>
2024-10-06 15:26:07 +0530

    images added

--- src/Pages/ServicePage/PortfolioCards.jsx
+++ src/Pages/ServicePage/PortfolioCards.jsx
@@ -1,11 +1,73 @@
-import React from "react";
-
 export const PortfolioCards = () => {
   return (
     <>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod quam, a
-      rerum perspiciatis ipsam sapiente recusandae eius hic at assumenda fugit,
-      fuga commodi tempora maxime! Consequatur perferendis quam quas ipsam?
+      <div className="flex gap-5 flex-col  md:flex-row w-[88.5%] mx-auto md:w-1/2 rounded-lg overflow-hidden group">
+        <div className="relative bg-service1 bg-center h-[450px] bg-cover bg-no-repeat w-full transition-transform duration-500 group-hover:scale-105 overflow-hidden">
+          <div className="absolute bottom-0 left-0 flex items-center justify-between p-10">
+            <div className="text-center text-[#68A4C4] bg-white bg-opacity-85 rounded-lg  p-8 font-roboto hover:bg-opacity-100 transition duration-150 ease-in">
+              <a
+                className="list-none inline-block cursor-pointer text-2xl font-semibold"
+                href="#"
+              >
+                Eosle Commodi
+              </a>
+              <p className="mt-2 text-gray-600">
+                Ut autem aut autem non a. Sint sint sit facilis nam iusto sint.
+                Libero corrupti neque eum hic non ut nesciunt dolorem.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="relative bg-service2 bg-center h-[450px] bg-cover bg-no-repeat w-full transition-transform duration-500 group-hover:scale-105 overflow-hidden">
+          <div className="absolute bottom-0 left-0 flex items-center justify-between p-10">
+            <div className="text-center text-[#68A4C4] bg-white bg-opacity-85 rounded-lg  p-8 font-roboto hover:bg-opacity-100 transition duration-150 ease-in">
+              <a
+                className="list-none inline-block cursor-pointer text-2xl font-semibold"
+                href="#"
+              >
+                Eosle Commodi
+              </a>
+              <p className="mt-2 text-gray-600">
+                Ut autem aut autem non a. Sint sint sit facilis nam iusto sint.
+                Libero corrupti neque eum hic non ut nesciunt dolorem.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="relative bg-service3 bg-center h-[450px] bg-cover bg-no-repeat w-full transition-transform duration-500 group-hover:scale-105 overflow-hidden">
+          <div className="absolute bottom-0 left-0 flex items-center justify-between p-10">
+            <div className="text-center text-[#68A4C4] bg-white bg-opacity-85 rounded-lg  p-8 font-roboto hover:bg-opacity-100 transition duration-150 ease-in">
+              <a
+                className="list-none inline-block cursor-pointer text-2xl font-semibold"
+                href="#"
+              >
+                Eosle Commodi
+              </a>
+              <p className="mt-2 text-gray-600">
+                Ut autem aut autem non a. Sint sint sit facilis nam iusto sint.
+                Libero corrupti neque eum hic non ut nesciunt dolorem.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="relative bg-service4 bg-center h-[450px] bg-cover bg-no-repeat w-full transition-transform duration-500 group-hover:scale-105 overflow-hidden">
+          <div className="absolute bottom-0 left-0 flex items-center justify-between p-10">
+            <div className="text-center text-[#68A4C4] bg-white bg-opacity-85 rounded-lg  p-8 font-roboto hover:bg-opacity-100 transition duration-150 ease-in">
+              <a
+                className="list-none inline-block cursor-pointer text-2xl font-semibold"
+                href="#"
+              >
+                Eosle Commodi
+              </a>
+              <p className="mt-2 text-gray-600">
+                Ut autem aut autem non a. Sint sint sit facilis nam iusto sint.
+                Libero corrupti neque eum hic non ut nesciunt dolorem.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      ;
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,10 @@ export default {
     extend: {
       backgroundImage: {
         banner: "url('/src/assets/img/hero-bg.jpg')",
+        service1: "url('/src/assets/img/services-1.jpg')",
+        service2: "url('/src/assets/img/services-2.jpg')",
+        service3: "url('/src/assets/img/services-3.jpg')",
+        service4: "url('/src/assets/img/services-4.jpg')",
       },
     },
   },
